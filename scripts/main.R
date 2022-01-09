bd2a38e71ef93e9dd269e1b783451ce0560d7ba9
Dominika Maziec <domz@MobicaPL.local>
2022-01-09 11:03:40 +0100

    RRF analysis

--- scripts/main.R
+++ scripts/main.R
@@ -1,11 +1,19 @@
+
 source(here::here('scripts', 'utils.R'))
 loadPackages()
 
 set.seed(93274)
 
 dataSet <- prepareData()
-#head(dataSet)
 
+
+
+run_plots(dataSet, "_unbalanced")
+dataSet <-  SMOTE(dataSet[,-31], dataSet$Class,K = 5)$data
+names(dataSet)[names(dataSet) == 'class'] <- 'Class'
+
+run_plots(dataSet, "_balanced")
+runRRF(dataSet)
 
 getSummary("out.csv", dataSet)
 # summary(dataSet)
--- scripts/utils.R
+++ scripts/utils.R
@@ -1,4 +1,20 @@
+install.packages("caret")
+install.packages("smotefamily")
+install.packages("RRF")
+install.packages("pROC")
+install.packages("ROSE")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("PRROC")
+
+library(dplyr)
+library(ggplot2)
+library(caret)
+library(smotefamily)
+library(RRF)
+library(pROC)
 library(datasets)
+library(PRROC)
 if (!require("pacman")) install.packages("pacman")
 
 #'Funkcja ładuje wszsytkie potrzebne biblioteki. Używa do tego menadżera packman.
@@ -8,7 +24,10 @@ loadPackages <- function(){
   #library(datasets)
   pacman::p_load(pacman, here, psych) 
   source(here::here('scripts', 'fileProcessing.R'))
+  source(here::here('scripts', 'runRRF.R'))
+  source(here::here('scripts', 'plots.R'))
 }
+
 
 #'Czyści konsolę
 #'
@@ -29,4 +48,22 @@ terminate<- function(clearConsole=TRUE){
   env <- foreach:::.foreachGlobals
   rm(list=ls(name=env), pos=env)
   cat("\014")
+}
+
+kfold_cv <- function(dataSet, i, n ) {
+  dataSet<-dataSet[sample(nrow(dataSet)),]
+  
+  folds <- cut(seq(1,nrow(dataSet)),breaks=n,labels=FALSE)
+  testIndexes <- which(folds==i,arr.ind=TRUE)
+  testData <- dataSet[testIndexes, ]
+  trainData <- dataSet[-testIndexes, ]
+  
+  list(testData, trainData)
+}
+
+plot_AUPRC <- function(testData, predicted, title) {
+  fg <- predicted[testData$Class==1]
+  bg <- predicted[testData$Class==0]
+  pr <- pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
+  plot(pr, main=title)
 }
