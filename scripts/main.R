076c8fb89277bf71b7f87390fc88ab0c4ea82edf
Dominika Maziec <domz@MobicaPL.local>
2022-01-22 23:39:38 +0100

    wip outForest

--- scripts/main.R
+++ scripts/main.R
@@ -5,23 +5,27 @@ source(here::here('scripts', 'utils.R'))
 loadPackages()
 
 set.seed(93274)
-#quit()
 
-#
-# Uwaga na zużycie pamięci w R. W moim przypadku procesy R nie zamknęły się nawet po zamknięciu RStudio.
-# Trzeba to zrobić ręcznie.
-#
-#
+dataSet <- prepareData(TRUE)
 
 dataSet <- prepareData(TRUE, TRUE)
+run_plots(dataSet, "_unbalanced")
+dataSet <-  SMOTE(dataSet[,-31], dataSet$Class,K = 5)$data
 
-#run_plots(dataSet, "_unbalanced")
 
-dataSet <-  smotefamily::SMOTE(dataSet[, -31], dataSet$Class, K = 5)$data
- names(dataSet)[names(dataSet) == 'class'] <- 'Class'
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+model <- train(Class~., data=dataSet, method="RRF", trControl=control)
+importance <- varImpPlot(model, scale=FALSE)
+print(importance)
+plot(importance)
 
-#run_plots(dataSet, "_balanced")
-#runRRF(dataSet)
+
+
+names(dataSet)[names(dataSet) == 'class'] <- 'Class'
+
+run_plots(dataSet, "_balanced")
+runRRF(dataSet)
+
 
 #getSummary("out.csv", dataSet)
 # summary(dataSet)
@@ -39,19 +43,12 @@ tmp <- experimentSVM(concatenateDatasets(t0, t1), newSVMTwoC, folderName='outTwo
 tmp2 <- experimentSVM(t0, newSVMOneC, folderName='outOne', addValidDataset=concatenateDatasets(t0_2, t1), 
     gamma=list(8, 4, 2, 1, 0.5, 0.01, 0.001, 0.0001), nu=list(0.5, 0.3, 0.15, 0.1, 0.05, 0.01))
 
-#svm <- newSVMOneC(x=dataSet, y=unlist(labels(dataSet)[2]), gamma=0.5, nu=0.5)
-#svm <- newSVMOneC(dataSet, gamma=0.5, nu=0.5)
-# rpusvm(Class ~ ., data=dataSet, type='one-classification', kernel='radial', gamma=0.5, nu=0.5)
-#terminate(FALSE, TRUE)
-
-#library(devtools)
-#devtools::install_github("h2oai/h2o4gpu", subdir = "src/interface_r")
 
 
-#getSummary("out.csv", dataSet)
+
+runOutForest(dataSet)
+getSummary("out.csv", dataSet)
 # summary(dataSet)
 
-#terminate(FALSE)
+#terminate(TRUE)
 
-#clear()
-#clear()
